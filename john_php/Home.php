b537046076dca5bc89927a07dfe1e2cb8bf18eee
chethan s - 1GV19CS013 <chethanachar81@gmail.com>
2024-02-08 10:42:10 +0530

    First commit

---
+++ john_php/Home.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Portfolio Page Template</title>
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap-4.4.1.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">John The Recruiter</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="../home.html">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../Signup.html">SignUp&nbsp;</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../View_company.html">View companie's&nbsp;</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../login.php">Login&nbsp; &nbsp;</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <section>
+      <div class="jumbotron text-center mt-2">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <h1>About John&nbsp;&nbsp;</h1>
+              <p>John is a recruiter who works of the companies to help the recruit the human resource's , If you're john you can login with your details and Manage this site.</p>
+              <p>&nbsp;</p>
+<p><a class="btn btn-primary btn-lg" href="#" role="button">Know more about John »</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <h1>John's achivement's&nbsp; &nbsp;</h1>
+            <p>John has been in the corprate IT world since 2011 and has hired over 2000+ qualified employee for 50+ IT&nbsp; Companie's&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container mt-2">
+        <div class="row">
+          <div class="col-md-4 col-12">
+            <ul class="list-unstyled">
+              <li class="media">
+                <img src="../images/Oracle.png" alt="Generic placeholder image" width="50" height="50" class="mr-3">
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">Building IT team at Oracle&nbsp; &nbsp;&nbsp;</h5>
+                  <div>&nbsp;</div>
+                  <p>in 2015 sucessed in building a team with 25 quality engineer'in oracle&nbsp; &nbsp;</p>
+                  
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-12">
+            <ul class="list-unstyled">
+              <li class="media">
+                <img src="../images/infosis.png" alt="Generic placeholder image" width="50" height="50" class="mr-3">
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">Leading 20+ teams at infosis&nbsp;&nbsp;</h5>
+                  <div>&nbsp;</div>
+                  <p>John worked for infosis for 5 years with 20+ teams with decipline and sensiority over 5 years&nbsp; &nbsp; &nbsp; &nbsp;</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-12">
+            <ul class="list-unstyled">
+              <li class="media">
+                <img src="../images/ai.png" alt="Generic placeholder image" width="50" height="50" class="mr-3">
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">&nbsp;Research in AI</h5>
+                  <div>&nbsp;</div>
+                  <p>John is also Passionated in AI and robotics , hence john have good recrord number of achivements in AI and Robotics.&nbsp; &nbsp; &nbsp; &nbsp;</p>
+                  
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <h2>Latest Work</h2>
+            <p>Now Working as an freelancer and IT recruiter&nbsp; &nbsp;</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-12 text-center">
+            <img src="../images/win.jpg" alt="" width="400" height="400" class="img-fluid">
+            <h4>Best Recruiter Award</h4>
+            <h6>2021&nbsp;</h6>
+          </div>
+          <div class="col-md-6 col-12 text-center mt-md-0 mt-2">
+            <img src="../images/win.jpg" alt="" width="400" height="400" class="img-fluid">
+            <h4>Best Enterpreneur award</h4>
+            <h6>2019&nbsp;</h6>
+          </div>
+        </div>
+        <hr>
+      </div>
+    </section>
+    <hr>
+    <section>
+      <div class="container">
+
+      </div>
+    </section>
+    <hr>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-12">
+            <h3 class="text-center">WHO WE ARE</h3>
+            <p>The project is done Using Dreamweaver 2021 as a part of Project assignment&nbsp; &nbsp;</p>
+          </div>
+          <div class="col-md-4 col-12">
+            <h3 class="text-center">GET IN TOUCH</h3>
+            <address class="text-center">
+            <strong>John , the recruiter</strong>
+            </address>
+            <address class="text-center">
+            John@gmail.com
+            </address>
+            <address class="text-center">
+            +919742429718&nbsp;
+            </address>
+<address class="text-center">
+        Banglore,&nbsp;&nbsp;
+        </address>
+          </div>
+          <div class="col-md-4 col-12">
+            <h3 class="text-center">NEWSLETTER</h3>
+            <form>
+              <div class="form-group col-12">
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                <button type="submit" class="btn btn-dark mt-2">Subscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>The project is done Using Dreamweaver 2021 as a part of Project assignment</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
